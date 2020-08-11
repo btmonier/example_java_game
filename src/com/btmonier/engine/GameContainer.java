9e9ecae3b167a1420d03e6f6305ada546a1f029a
Brandon <brandon.monier@gmail.com>
2020-08-11 12:48:40 -0400

    Start fresh

--- src/com/btmonier/engine/GameContainer.java
+++ src/com/btmonier/engine/GameContainer.java
@@ -1,127 +1,14 @@
 package com.btmonier.engine;
 
-public class GameContainer implements Runnable {
-    private Thread thread;
-    private Window window;
-    private Renderer renderer;
-
-    private boolean running = false;
-    private final double UPDATE_CAP = 1.0 / 60.0;
-    private int width = 320, height = 240;
-    private float scale = 1f;
-    private String title = "MonierEngine v0.1";
-
-    public GameContainer() {
-
-    }
-
-    public void start() {
-        window = new Window(this);
-        renderer = new Renderer(this);
-        thread = new Thread(this);
-        thread.run();
-    }
-
-    public void stop() {
-
-    }
-
-    public void run() {
-        running = true;
-
-        double convert = 1e9;
-
-        boolean render = false;
-        double firstTime = 0;
-        double lastTime = System.nanoTime() / convert;
-        double passedTime = 0;
-        double unprocessedTime = 0;
-
-        double frameTime = 0;
-        int frames = 0;
-        int fps = 0;
-
-        while(running) {
-            render = false;
-            firstTime = System.nanoTime() / convert;
-            passedTime = firstTime - lastTime;
-            lastTime = firstTime;
-
-            unprocessedTime += passedTime;
-            frameTime += passedTime;
-
-            while(unprocessedTime >= UPDATE_CAP) {
-                unprocessedTime -= UPDATE_CAP;
-                render = true;
-
-                // TODO: Update game
-                if (frameTime >= 1.0) {
-                    frameTime = 0;
-                    fps = frames;
-                    frames = 0;
-                    System.out.println("FPS: " + fps);
-                }
-            }
-
-            if (render) {
-                renderer.clear();
-                // TODO: render game
-                window.update();
-                frames++;
-            } else {
-                try {
-                    Thread.sleep(1);
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                }
-            }
-        }
-        dispose();
-    }
-
-    private void dispose() {
-
-    }
+public class GameContainer {
 
     public static void main(String[] args) {
-        GameContainer gc = new GameContainer();
-        gc.start();
+        String title = "MonierEngine v0.1";
+        System.out.println("Launching " + title + "...");
+
+        Game game = new Game(title, 400, 400);
+        game.start();
     }
 
-    public int getWidth() {
-        return width;
-    }
-
-    public void setWidth(int width) {
-        this.width = width;
-    }
-
-    public int getHeight() {
-        return height;
-    }
-
-    public void setHeight(int height) {
-        this.height = height;
-    }
-
-    public float getScale() {
-        return scale;
-    }
-
-    public void setScale(float scale) {
-        this.scale = scale;
-    }
-
-    public String getTitle() {
-        return title;
-    }
-
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
-    public Window getWindow() {
-        return window;
-    }
 }
 
