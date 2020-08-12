c8af7b37909913c4ee15d2542c59a7ef0069db01
Brandon <brandon.monier@gmail.com>
2020-08-12 17:04:57 -0400

    Add level loading concept

--- src/com/btmonier/engine/state/GameState.java
+++ src/com/btmonier/engine/state/GameState.java
@@ -3,25 +3,29 @@ package com.btmonier.engine.state;
 import com.btmonier.engine.Game;
 import com.btmonier.engine.entities.Player;
 import com.btmonier.engine.tiles.Tile;
+import com.btmonier.engine.worlds.World;
 
 import java.awt.*;
 
 public class GameState extends State {
     private Player player;
+    private World world;
+
     public GameState(Game game) {
         super(game);
         player = new Player(game, 100, 100);
+        world = new World("res/worlds/world_01.txt");
     }
 
     @Override
     public void tick() {
+        world.tick();
         player.tick();
     }
 
     @Override
     public void render(Graphics g) {
-        Tile.tiles[0].render(g, 0, 0);
-        Tile.tiles[1].render(g, 32, 0);
+        world.render(g);
         player.render(g);
     }
 }
