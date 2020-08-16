f19b4c5c0457e7f03980096fd0d53c021522ed2f
Brandon <brandon.monier@gmail.com>
2020-08-16 18:12:45 -0400

    Test water tiles

--- src/com/btmonier/engine/tiles/WaterTile.java
+++ src/com/btmonier/engine/tiles/WaterTile.java
@@ -6,7 +6,7 @@ import java.awt.image.BufferedImage;
 
 public class WaterTile extends Tile {
     public WaterTile(int id) {
-        super(Assets.waterMed, id);
+        super(Assets.waterLight, id);
     }
 
     @Override
